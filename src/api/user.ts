c115c2dadcf740b0b55fdbe20f8826e123d6521a
李寻欢 <lxh@cxh.cn>
2023-07-27 15:58:08 +0800

    feat: 新增用户管理

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,4 +1,5 @@
 import { http } from "@/utils/http";
+import { BasePageResult } from "@/utils/http/base";
 
 /*** 修改密码参数 */
 export type ChangePasswordParam = {
@@ -10,7 +11,43 @@ export type ChangePasswordParam = {
   confirmPassword?: string;
 };
 
+// 分页查询用户列表参数
+export type PageUserParam = {
+  // 页码
+  current: number;
+  // 页数量
+  size: number;
+};
+
+// 分页查询用户返回列表数据
+export type PageUserResult = {
+  id: string; // Id
+  username: string; // 用户名
+  email: string; // 邮箱
+  isVerified: boolean; // 是否验证邮箱
+  role: string; // 角色
+  createdAt: Date; // 注册时间
+  lastLoginAt?: Date; // 最后登录时间
+  lastLoginIp?: string; // 最后登录IP
+  bindJdCount: number; // 京东账号数
+  bindNotifyCount: number; // 通知渠道数
+};
+
 // 修改密码
 export const changePassword = (data: ChangePasswordParam) => {
   return http.request<object>("post", "/admin/v1/user/password", { data });
+};
+
+// 分页查询用户列表
+export const pageUser = (params: PageUserParam) => {
+  return http.request<BasePageResult<PageUserResult>>(
+    "get",
+    "/admin/v1/user/page",
+    { params }
+  );
+};
+
+// 删除用户
+export const deleteUser = (id: string) => {
+  return http.request<object>("delete", "/admin/v1/user/" + id);
 };
